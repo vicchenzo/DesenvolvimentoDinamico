31463dfad12791590e77ad0f974108c44e950c95
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-01-23 23:03:52 -0300

    Create 05_operadores lógicos.js

---
+++ 05_operadores lógicos.js
@@ -0,0 +1,64 @@
+/*
+OPERADORES LÓGICOS
+Operações lógicas em valores booleanos
+E: &&
+OU: ||
+NÃO: !
+
+Operações de comparação, aritméticos lógicos
+== Igual à
+!= Diferente
+
+=== Mesmo valor e mesmo tipo
+!== Diferente em valor e tipo
+
+< menor que
+> maior que
+
+<= menor ou igual a
+>= maior ou igual à
+
+QUANDO ENTRA O && 'LÓGICO' APENAS QUANDO AS DUAS CONDIÇÕES SÃO VERDADEIRAS ELE É VERDADEIRO
+
+
+X               Y               X && Y
+VERDADEIRO      VERDADEIRO      VERDADEIRO
+VERDADEIRO      FALSO           FALSO
+FALSO           VERDADEIRO      FALSO
+FALSO           FALSO           FALSO
+*/
+
+//Operador E lógico[&&]
+const a = true;
+const b = false;
+
+const resultadoE1 = a && b; // False
+const resultadoE2 = a && a; // true
+
+console.log(`true && false: ${resultadoE1}`)
+console.log(`true && true: ${resultadoE2}`)
+
+//Operador OU lógico[||]
+const resultadoOU1 = a||b; //true
+const resultadoOU2 = a||a; // false
+
+console.log(`true || false: ${resultadoOU1}`)// saída: false
+console.log(`false || false: ${resultadoOU2}`)// saída: true
+
+//Definicao de operadores em programacao
+//Indica que vai acontecer uma operação na sua variável
+//Simbolos ou palavras-chave especiais que permitem realziar operações em valores, variáveis ou expressões;
+
+//Operadores Aritiméticos
+/*
+adição (+)
+subtração (-)
+multiplicação (*)
+divisão (/)
+
+modulo(%): o valor do resto da divisão
+exponencial(**):
+incrementar(++): aumenta uma unidade do valor referido
+decrementar(--): diminui uma unidade do valor referido
+*/
+
