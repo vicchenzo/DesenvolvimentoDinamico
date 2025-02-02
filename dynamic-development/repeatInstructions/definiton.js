2f25be55cc7ca29564b8954b33c444492912fa63
ᴠɪᴄ. <106128743+vicchenzo@users.noreply.github.com>
2025-02-01 23:41:31 -0300

    Create definiton.js

---
+++ dynamic-development/repeatInstructions/definiton.js
@@ -0,0 +1,15 @@
+/*
+Blocos de código executados enquanto uma condição específica é verdadeira
+Variavel que representa função do tempo
+Pode ser 'rainy' (chuvoso), "sunny" (ensolarado) ou cloudy (nublado)
+=== IGUALDADE ESTRITA
+*/
+
+let weatherForecast = 'rainy';
+//Analagoia com a avida real: Decidindo se devemos levar um guarda-chuva
+
+if (weatherForecast === 'rainy') {
+    console.log("Levar Guarda chuva!")
+} else {
+    console.log("Não levar o guarda chuva!")
+}
